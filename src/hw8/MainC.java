c38dcbffefa625d0630a4913112349923fef0bb8
Christina <Christina.Kohl@student.uibk.ac.at>
2016-05-16 18:39:40 +0200

    variable

--- src/hw8/MainC.java
+++ src/hw8/MainC.java
@@ -9,6 +9,7 @@ public class MainC {
 
     public static void main(String[] args) {
 
+        int n = 100;
         int m = 12;
         int max = (int) Math.pow(2,m);
         Random rnd = new Random();
@@ -16,11 +17,11 @@ public class MainC {
         ChordNode firstNode = new ChordNode(rnd.nextInt(max), m);
         firstNode.join(null);
 
-        ChordNode[] nodes = new ChordNode[100];
+        ChordNode[] nodes = new ChordNode[n];
         nodes[0] = firstNode;
         System.out.println("First node is " + firstNode.getID());
 
-        for(int i = 1; i < 100; i++) {
+        for(int i = 1; i < n; i++) {
             int id = rnd.nextInt(max);
             while(firstNode.findSuccessor(id).getID() == id) { // if the ID is already taken, generate a new one
                 id = rnd.nextInt(max);
@@ -34,15 +35,15 @@ public class MainC {
 
         int maxHops = 0;
         double avgHops = 0;
-        for(int i = 0; i < 100; i++) {
-            for(int j = 0; j < 100; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 int hops = nodes[i].sendMSG(nodes[j].getID(), "");
                 if(hops > maxHops) maxHops = hops;
-                avgHops = avgHops + (double)hops/10000;
+                avgHops = avgHops + (double)hops/(n*n);
             }
         }
         System.out.println("average Hops: " + avgHops);
-        System.out.println("maxHops Hops:" + maxHops);
+        System.out.println("maxHops Hops: " + maxHops);
     }
 
 }
